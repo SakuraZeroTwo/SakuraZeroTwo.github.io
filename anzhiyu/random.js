f2a485eb12bc713df99d6083fdb164f0c4bf34b5
SakuraZeroTwo <1027309551@qq.com>
2025-04-21 17:37:39 +0800

    Site updated: 2025-04-21 17:37:39

--- anzhiyu/random.js
+++ anzhiyu/random.js
@@ -1,3 +1,3 @@
-var posts=["2025/04/20/设计模式-抽象工厂/","2025/04/20/测试文章/","2025/04/21/设计模式-工厂方法/"];function toRandomPost(){
+var posts=["2025/04/20/测试文章/","2025/04/21/设计模式-原型方法/","2025/04/20/设计模式-抽象工厂/","2025/04/21/设计模式-工厂方法/"];function toRandomPost(){
     pjax.loadUrl('/'+posts[Math.floor(Math.random() * posts.length)]);
   };
